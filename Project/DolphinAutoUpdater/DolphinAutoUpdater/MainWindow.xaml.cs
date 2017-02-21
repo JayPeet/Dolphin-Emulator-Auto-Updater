ed739c6ced3e0ffbe5a4775dd66fc82a8463811f
Jay Peet <jaytechmedia@googlemail.com>
2017-02-21 19:20:48 +0000

    Begin work on rewrite.

---
+++ Project/DolphinAutoUpdater/DolphinAutoUpdater/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO;
+using System.Xml;
+
+namespace DolphinAutoUpdater
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            info_label.Content = "Initializing...";
+            LoadConfig();
+        }
+
+        /// <summary>
+        /// Loads the configuration file, or creates one.
+        /// </summary>
+        private void LoadConfig()
+        {
+            string xmlConfig = "";
+            //Create config file, if missing.
+            if (!File.Exists("config.xml"))
+            {
+                //Open new config file and fill with data.
+                StreamWriter w = new StreamWriter("config.xml");
+                xmlConfig = "<config><installDir>" + AppDomain.CurrentDomain.BaseDirectory + "</installDir><installedVersion>0</installedVersion><devBuilds>0</devBuilds></config>";
+
+                w.Write(xmlConfig);
+                w.Close();
+            }
+
+            //Load config from file.
+            XmlDocument configDoc = new XmlDocument();
+            configDoc.Load("config.xml");
+            info_label.Content = configDoc["config"]["installDir"].InnerText;
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,13 +1,2 @@
-# Dolphin Auto Updater
-An automatic updater for the Dolphin Emulator.<br>
-##TODO
--Add Error checking and handlers<br>
--Prompt the user when an update is available, instead of just updating.<br>
--Improve stable build updating. Right now, it downloads the new stable installer, then runs it. The installers files CAN be extracted with 7zip...Just not with 7zips command line...<br>
--Improve the info displayed while downloading updates.<br>
--Display Update information.<br>
--Sort out HtmlAgilityPack reference.<br>
--Linux and OSX versions?<br>
--Add nice comments to the code.<br>
-###Notes
-Pressing shift when launching will display the config window.
+# Dolphin Auto Updater 2.0
+A Rewrite of the first version. Working on Improving this into a finished project.<br>
